e856f0e99b7b6780053bd3fa8733785c0bf320a5
dadurand <ha.halo@live.fr>
2019-10-28 14:32:34 +0100

    fin

--- static/script.js
+++ static/script.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
 
+    $("#pass").keydown(function () {
+
+        $("#pass").prop("title", "BOjour")
+
+        if ($("#pass").val().length < 4) {
+            $(".progress-bar").css("width", "0%")
+        }
+        else if ($("#pass").val().length == 4) {
+            $(".progress-bar").css("width", "25%")
+            $(".progress-bar").css("background", "red")
+        }
+        else if ($("#pass").val().length == 6) {
+            $(".progress-bar").css("width", "75%")
+            $(".progress-bar").css("background", "orange")
+        }
+        else if ($("#pass").val().length == 9) {
+            $(".progress-bar").css("width", "100%")
+            $(".progress-bar").css("background", "green")
+        }
+
+    })
+
+
     // Nombre de caractere requis
     $("form").submit(function () {
         var nombreCaractere = false;
@@ -9,7 +32,7 @@ $(document).ready(function () {
                 $("input:eq(" + i + ")").css("border-color", "red");
             }
 
-            else if ($("input:eq(" + i + ")").val().length <= 10){
+            else if ($("input:eq(" + i + ")").val().length <= 10) {
                 $("input:eq(" + i + ")").css("border-color", "green");
             }
 
@@ -19,17 +42,17 @@ $(document).ready(function () {
         }
         // Fin nombre de caractere requis
         // Nombre maximum de caracteres
-        if ($(".password > input, .login > #pseudo").val().length > 10){
+        if ($(".password > input, .login > #pseudo").val().length > 10) {
             alert("Le pseudo et/ou le mot de passe est trop long");
             $(".password > input, .login > #pseudo").css("border-color", "red");
         }
         //Fin nombre de caracetres maximum
         // Vérification Mot de passe
-        
-        if ($("#pass").val() != $("#passconf").val()){
+
+        if ($("#pass").val() != $("#passconf").val()) {
             alert("Mot de passe incorrect");
             $(".password > input").css("border-color", "red");
-            
+
         }
         //Fin verification mot de passe 
         // Verification Email
@@ -44,7 +67,7 @@ $(document).ready(function () {
         // Fin verification email
 
         // Sécurité mot de passe 
-        
+
         return false;
     })
 
